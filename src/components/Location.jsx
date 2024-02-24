bfa7eb50ec312e9d235a95b95a56e9b0adb52fe8
박시은 <charm516@naver.com>
2024-02-24 15:59:05 +0900

    feat: react-kakao-maps-sdk 적용 및 컨트롤 기능 구현

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,27 +1,34 @@
 import styled from 'styled-components';
-import { useEffect } from 'react';
+import { Map, MapTypeControl, ZoomControl } from 'react-kakao-maps-sdk';
 const { kakao } = window;
 
 const Location = () => {
-  useEffect(() => {
-    var container = document.getElementById('map'); // 지도를 표시할 div
-    var options = {
-      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488), // 지도의 중심좌표
-      level: 3 // 지도의 확대 레벨
-    };
-    var map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
-  }, []);
-
   return (
-    <div>
-      <StLocationContainer id="map"></StLocationContainer>
-    </div>
+    <StMapContiner>
+      <StMapSize // 지도를 표시할 Container
+        id="map"
+        center={{
+          // 지도의 중심좌표
+          lat: 33.450701,
+          lng: 126.570667
+        }}
+        level={3} // 지도의 확대 레벨
+      >
+        {/* 지도에 컨트롤 올리기 */}
+        <ZoomControl position={kakao.maps.ControlPosition.TOPRIGHT} />
+        <MapTypeControl position={kakao.maps.ControlPosition.TOPRIGHT} />
+      </StMapSize>
+    </StMapContiner>
   );
 };
 
-const StLocationContainer = styled.div`
-  width: 500px;
-  height: 400px;
+const StMapContiner = styled.div`
+  height: 100vh;
+`;
+
+const StMapSize = styled(Map)`
+  width: 100%;
+  height: 100%;
 `;
 
 export default Location;
