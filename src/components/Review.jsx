6ff188e87b2e8f4ce96b86c13b706427f10d97d9
Jiwon <tj1tj2tj3@naver.com>
2024-02-25 22:21:52 +0900

    feat: 평점 유효성 검사 추가

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -9,6 +9,7 @@ import { FaRegTrashAlt } from 'react-icons/fa';
 import { FaRegStar } from 'react-icons/fa';
 import { FaStar } from 'react-icons/fa';
 import reviewApi from 'api/reviewApi';
+import { AiOutlineExclamationCircle } from 'react-icons/ai';
 
 const Review = () => {
   const [gradeStar, setGradeStar] = useState(0);
@@ -16,6 +17,8 @@ const Review = () => {
   const [reviews, setReviews] = useState([]);
   const [reviewContent, setReviewContent] = useState('');
   const textArea = useRef();
+  const gradeWrap = useRef();
+  const [isGradeInvalid, setIsGradeinvalid] = useState(false);
 
   useEffect(() => {
     const loadReviews = async () => {
@@ -31,13 +34,6 @@ const Review = () => {
   //생성된 별 아이콘을 클릭했을때 handleStarIconClick가 실행되고 그 클릭된 별아이콘의 인덱스값으로 gradeStar가 set됨
   const Star = ({ selected = false, handleStarIconClick }) => {
     return <FaStar color={selected ? 'gold' : 'grey'} onClick={handleStarIconClick} />;
-  };
-
-  const handleDeleteReview = () => {
-    if (window.confirm('리뷰를 삭제하시겠습니끼?')) {
-    } else {
-      alert('삭제취소');
-    }
   };
 
   const handleModalClose = () => {
@@ -75,6 +71,11 @@ const Review = () => {
       textArea.current.focus();
       return;
     }
+    if (gradeStar === 0) {
+      alert('평점을 선택해주세요.');
+      setIsGradeinvalid(true);
+      return;
+    }
 
     const newRivew = {
       id: crypto.randomUUID(),
@@ -93,11 +94,27 @@ const Review = () => {
     }
   };
 
+  const handleDeleteReview = async (id) => {
+    if (window.confirm('리뷰를 삭제하시겠습니끼?')) {
+      try {
+        await reviewApi.delete(`/${id}`);
+        alert('리뷰가 삭제되었습니다.');
+      } catch (error) {}
+    } else {
+      alert('삭제취소');
+    }
+  };
+
   const randomBrightColor = () => {
     const colorR = Math.floor(Math.random() * 128 + 128).toString(16);
     const colorG = Math.floor(Math.random() * 128 + 128).toString(16);
     const colorB = Math.floor(Math.random() * 128 + 128).toString(16);
     return `#${colorR + colorG + colorB}`;
+  };
+
+  const handleStarIconClick = (idx) => {
+    setGradeStar(idx);
+    setIsGradeinvalid(false);
   };
 
   return (
@@ -110,18 +127,21 @@ const Review = () => {
           placeholder="리뷰를 작성해주세요."
         />
         <StDropdownFormButtonWrap>
-          <StGradeDropdown>
+          <StGradeWrap ref={gradeWrap}>
             <span style={{ marginRight: '3px' }}>평점</span>
-            <StStarContainer>
+            <StStarContainer style={{ marginRight: '3px' }}>
               {/* Star라는 컴포넌트 5개가 만들어짐 */}
               {/* selected 프롭스는 gradeStar가 index보다 크면  true가 됨 */}
               {/* handleStarIconClick 함수 프롭스도 넘겨줌 */}
               {[1, 2, 3, 4, 5].map((idx) => {
-                return <Star key={idx} selected={gradeStar >= idx} handleStarIconClick={() => setGradeStar(idx)} />;
+                return (
+                  <Star key={idx} selected={gradeStar >= idx} handleStarIconClick={() => handleStarIconClick(idx)} />
+                );
               })}
               {/* <FaStar color={selected ? 'gold' : 'grey'} onClick={handleStarIconClick} />; */}
             </StStarContainer>
-          </StGradeDropdown>
+            {isGradeInvalid && <AiOutlineExclamationCircle color={'red'} />}
+          </StGradeWrap>
 
           <StReviewFormBottom onClick={handleAddReviewButton}>등록</StReviewFormBottom>
         </StDropdownFormButtonWrap>
@@ -155,7 +175,7 @@ const Review = () => {
                 <GoPencil style={{ marginRight: '3px' }} />
                 수정
               </li>
-              <li style={{ display: 'flex', padding: '10px' }} onClick={handleDeleteReview}>
+              <li style={{ display: 'flex', padding: '10px' }} onClick={() => handleDeleteReview(item.id)}>
                 <FaRegTrashAlt style={{ marginRight: '3px' }} />
                 삭제
               </li>
@@ -180,6 +200,8 @@ export const StReviewTapContainer = styled.div`
   color: ${colors.subColor};
   padding-bottom: 20px;
   position: relative;
+  height: 430px; /* 높이를 설정하여 컴포넌트가 스크롤 가능하도록 함 */
+  overflow-y: auto; /* 세로 스크롤을 활성화함 */
 `;
 
 export const StReviewFormContainer = styled.div`
@@ -203,9 +225,12 @@ export const StDropdownFormButtonWrap = styled.div`
   justify-content: space-between;
 `;
 
-export const StGradeDropdown = styled.div`
+export const StGradeWrap = styled.div`
   display: flex;
   font-size: 14px;
+  height: fit-content;
+  padding: 2px;
+  border-radius: 5px;
 `;
 
 export const StReviewFormBottom = styled.div`
