059ccb1c8c98452b30392a4b9ef3b97487935ee7
ji min <142903244+gggimmmin@users.noreply.github.com>
2024-02-26 07:02:47 +0900

    [feat] accessToken 요청, 응답 인터셉터 구현

--- src/api/index.js
+++ src/api/index.js
@@ -6,3 +6,27 @@ export const authApi = axios.create({
     'Content-Type': 'application/json'
   }
 });
+
+authApi.interceptors.request.use(
+  (config) => {
+    // 헤더에 토큰 넣기
+    const accsessToken = localStorage.getItem('accessToken');
+    if (accsessToken) {
+      config.headers['Authorization'] = `Bearer ${accsessToken}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+authApi.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (err) => {
+    console.log('err :', err);
+    return Promise.reject(err);
+  }
+);
