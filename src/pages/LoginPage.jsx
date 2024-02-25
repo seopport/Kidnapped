9736f157bf5f9d0ab20f411f0b6ae4e59302edd2
ji min <142903244+gggimmmin@users.noreply.github.com>
2024-02-26 07:25:08 +0900

    [refactor] 로그인 로직 thunk 기능 사용

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { login } from '../redux/modules/authSlice';
+import { __login } from '../redux/modules/authSlice';
 import styled from 'styled-components';
 import { toast } from 'react-toastify';
 import useForm from 'hooks/useForm';
@@ -20,20 +20,21 @@ const LoginPage = () => {
   const onSubmitHandler = async (event) => {
     event.preventDefault();
     if (isLoginMode) {
+      dispatch(__login({ id, password }));
       //로그인 처리
-      try {
-        const { data } = await authApi.post('/login?expiresIn=30m', {
-          id,
-          password
-        });
-        const { accessToken, nickname, avatar, userId } = data;
-        if (data.success) {
-          dispatch(login({ accessToken, nickname, avatar, userId }));
-          toast.success('로그인 성공!');
-        }
-      } catch (err) {
-        toast.error(err.response.data.message);
-      }
+      // try {
+      //   const { data } = await authApi.post('/login?expiresIn=30m', {
+      //     id,
+      //     password
+      //   });
+      //   const { accessToken, nickname, avatar, userId } = data;
+      //   if (data.success) {
+      //     dispatch(login({ accessToken, nickname, avatar, userId }));
+      //     toast.success('로그인 성공!');
+      //   }
+      // } catch (err) {
+      //   toast.error(err.response.data.message);
+      // }
     } else {
       // 회원가입 처리
       try {
--- src/redux/modules/authSlice.js
+++ src/redux/modules/authSlice.js
@@ -1,4 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { authApi } from 'api';
+import { toast } from 'react-toastify';
 
 const initialState = {
   isLogin: !!localStorage.getItem('accessToken'),
@@ -6,6 +8,23 @@ const initialState = {
   nickname: localStorage.getItem('nickname'),
   userId: localStorage.getItem('userId')
 };
+
+export const __login = createAsyncThunk('login', async ({ id, password }, thunkAPI) => {
+  try {
+    const { data } = await authApi.post('/login?expiresIn=30m', {
+      id,
+      password
+    });
+    const { accessToken, nickname, avatar, userId } = data;
+    if (data.success) {
+      toast.success('로그인 성공!');
+      return { accessToken, nickname, avatar, userId };
+    }
+  } catch (err) {
+    toast.error(err.response.data.message);
+    return thunkAPI.rejectWithValue(err);
+  }
+});
 
 const authSlice = createSlice({
   name: 'auth',
@@ -26,6 +45,28 @@ const authSlice = createSlice({
       state.isLogin = false;
       localStorage.clear();
     }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(__login.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(__login.fulfilled, (state, action) => {
+      const { accessToken, avatar, nickname, userId } = action.payload;
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('avatar', avatar);
+      localStorage.setItem('nickname', nickname);
+      localStorage.setItem('userId', userId);
+      state.isLogin = true;
+      state.avatar = avatar;
+      state.nickname = nickname;
+      state.userId = userId;
+      state.isLoading = false;
+    });
+    builder.addCase(__login.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.error = action.payload;
+    });
   }
 });
 
