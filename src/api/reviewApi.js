e59f34b9df84a4a2a52f6863435759b8f5404fca
Jiwon <tj1tj2tj3@naver.com>
2024-02-26 15:43:51 +0900

    setting: 라이브러리 설치

--- src/api/reviewApi.js
+++ src/api/reviewApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instace = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_REVIEWS__URL
+  baseURL: process.env.REACT_APP_SERVER_REVIEWS_URL
 });
 
 export default instace;
